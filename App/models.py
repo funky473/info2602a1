db825e8666ab5448f6ca20fa1c2e71df4ab2659f
funky473 <hsboxing19@gmail.com>
2025-02-22 17:00:04 -0400

    work so far

--- App/models.py
+++ App/models.py
@@ -4,12 +4,48 @@ db = SQLAlchemy()
 
 class UserPokemon(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+  userid = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+  pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemon.id'), nullable=False)
+  name = db.Column(db.String(255), nullable=False)
+
+  #user = db.relationship("User", back_populates="pokemons")
+  #pokemon = db.relationship("Pokemon", back_populates="owners")
   pass
 
 class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(255),nullable = False,unique=True)
+  email = db.Column(db.String(255),nullable = False,unique=True)
+  password = db.Column(db.String(255),nullable = False,unique=True)
+
+  #pokemons = db.relationship("UserPokemon", back_populates="user", cascade="all, delete-orphan")
+  
+  def __init__(self, username, email, password):
+    self.username = username
+    self.email = email
+    self.set_password(password)
+
+  def set_password(self, password):
+    """Create hashed password."""
+    self.password = generate_password_hash(password, method='scrypt')
+
+  def check_password(self, password):
+    """Check hashed password."""
+    return check_password_hash(self.password, password)
   pass
 
 class Pokemon(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(255), nullable=False,unique=True)
+  attack = db.Column(db.Integer, nullable=False)
+  defense = db.Column(db.Integer, nullable=False)
+  hp = db.Column(db.Integer, nullable=False)
+  height = db.Column(db.Float, nullable=True)
+  sp_attack = db.Column(db.Integer, nullable=False)
+  sp_defense = db.Column(db.Integer, nullable=False)
+  speed = db.Column(db.Integer, nullable=False)
+  type1 = db.Column(db.String(255), nullable=False)
+  type2 = db.Column(db.String(255), nullable=False)
+
+  #Trainers = db.relationship("UserPokemon", back_populates="pokemon", cascade="all, delete-orphan")
   pass
--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,26 @@ from App import app, initialize_db
 
 @app.cli.command("init", help="Creates and initializes the database")
 def initialize():
-  initialize_db()
-  print("Database Initialized!")
+    initialize_db()
+    db.drop_all()
+    db.create_all()
+    with open('pokemon.csv') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            
+            new_pokemon = Pokemon(
+              name=row['name'],
+              attack=int(row['attack']),
+              defense=int(row['defense']),
+              hp=int(row['hp']),
+              height=float(row['height_m']) if row['height_m'].isdigit() else 0,
+              sp_attack=int(row['sp_attack']),
+              sp_defense=int(row['sp_defense']),
+              speed=int(row['speed']),
+              type1=row['type1'],
+              type2=row['type2']
+                )
+            db.session.add(new_pokemon)
+    db.session.commit()
+    print("Database Initialized!")
   
